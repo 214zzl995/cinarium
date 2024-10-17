1cd7b3f1b009e86bc63d65b3262951f77af6a6e8
leri <214zzl995@gmail.com>
2024-10-17 22:06:35 +0800

    移植界面到新界面

--- lib/screens/settings/page/settings_crawler_template_page.dart
+++ lib/screens/settings/page/settings_crawler_template_page.dart
@@ -1,34 +1,98 @@
+import 'package:bridge/call_rust/task/crawler.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:reorderables/reorderables.dart';
 
 import '../../../components/scroll_animator.dart';
-import '../components/crawler_template_setting.dart';
+import '../controllers/settings_controller.dart';
 
 class SettingsCrawlerTemplatePage extends StatelessWidget {
   const SettingsCrawlerTemplatePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ScrollAnimator(
-        scrollSpeed: 0.5,
-        builder: (context, controller, physics) => ListView(
-          physics: physics,
-          controller: controller,
-          padding:
-          const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-          children: [
-            _buildMenuItemHeader(context, 'Appearance'),
-            const CrawlerTemplateSetting(),
-          ],
-        ));
-  }
-
-  Widget _buildMenuItemHeader(BuildContext context, String title) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 8, bottom: 3),
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.bodyMedium,
+    return Container(
+      child: Column(
+        children: [
+          Text('Crawler Templates'),
+          Expanded(child: _buildITemplatesList(context))
+        ],
       ),
     );
+  }
+
+  Widget _buildITemplatesList(BuildContext context) {
+    return ScrollAnimator(
+        scrollSpeed: 0.5,
+        builder: (context, controller, physics) => CustomScrollView(
+              controller: controller,
+              physics: physics,
+              slivers: <Widget>[
+                Selector<SettingsController, List<CrawlerTemplate>>(
+                  selector: (_, settings) => settings.crawlerTemplates,
+                  builder: (BuildContext context,
+                      List<CrawlerTemplate> crawlerTemplates, Widget? child) {
+                    return ReorderableSliverList(
+                      delegate: ReorderableSliverChildBuilderDelegate(
+                          (BuildContext context, int index) {
+                        final template = context
+                            .read<SettingsController>()
+                            .crawlerTemplates[index];
+                        return Container(
+                          key: ValueKey(template),
+                          height: 50,
+                          padding: const EdgeInsets.symmetric(horizontal: 16.0),
+                          margin: const EdgeInsets.symmetric(
+                              vertical: 4.0, horizontal: 14),
+                          decoration: BoxDecoration(
+                            borderRadius: const BorderRadius.all(
+                              Radius.circular(12),
+                            ),
+                            border: Border.all(
+                              color: Theme.of(context)
+                                  .colorScheme
+                                  .outlineVariant
+                                  .withOpacity(0.5),
+                              width: 1,
+                            ),
+                          ),
+                          child: Row(
+                            children: [
+                              Expanded(
+                                child: Text(template.baseUrl),
+                              ),
+                              Selector<SettingsController, bool>(
+                                  builder: (_, enabled, __) {
+                                    return Checkbox(
+                                        value: enabled,
+                                        onChanged: (value) {
+                                          context
+                                              .read<SettingsController>()
+                                              .modelDisableChange(value, index);
+                                        });
+                                  },
+                                  selector: (_, settings) =>
+                                      settings.crawlerTemplates[index].enabled),
+                            ],
+                          ),
+                        );
+                      }, childCount: crawlerTemplates.length),
+                      onReorder:
+                          context.read<SettingsController>().onTemplatesReorder,
+                      buildDraggableFeedback: (BuildContext context,
+                          BoxConstraints constraints, Widget child) {
+                        return SizedBox(
+                          height: 58,
+                          child: ConstrainedBox(
+                            constraints: constraints,
+                            child: child,
+                          ),
+                        );
+                      },
+                    );
+                  },
+                )
+              ],
+            ));
   }
 }
--- lib/screens/settings/page/settings_monitor_folder_page.dart
+++ lib/screens/settings/page/settings_monitor_folder_page.dart
@@ -1,34 +1,194 @@
 import 'package:flutter/material.dart';
+import 'package:material_symbols_icons/symbols.dart';
+import 'package:provider/provider.dart';
 
 import '../../../components/scroll_animator.dart';
 import '../components/crawler_template_setting.dart';
+import '../controllers/settings_controller.dart';
 
 class SettingsMonitorFolderPage extends StatelessWidget {
   const SettingsMonitorFolderPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return ScrollAnimator(
-        scrollSpeed: 0.5,
-        builder: (context, controller, physics) => ListView(
-          physics: physics,
-          controller: controller,
-          padding:
-          const EdgeInsets.only(left: 20, right: 20, bottom: 20),
-          children: [
-            _buildMenuItemHeader(context, 'Appearance'),
-            const CrawlerTemplateSetting(),
-          ],
-        ));
+    return Container(
+      child: Column(
+        children: [
+          Text('Monitor Folders'),
+          Expanded(child: _buildSearchFolderList(context))
+        ],
+      ),
+    );
   }
 
-  Widget _buildMenuItemHeader(BuildContext context, String title) {
-    return Padding(
-      padding: const EdgeInsets.only(top: 8, bottom: 3),
-      child: Text(
-        title,
-        style: Theme.of(context).textTheme.bodyMedium,
-      ),
+  Widget _buildSearchFolderList(BuildContext context) {
+    return Selector<SettingsController, List<String>>(
+      builder: (context, paths, child) => ScrollAnimator(
+      scrollSpeed: 0.5,
+      builder: (context, controller, physics) => ListView(
+        padding: const EdgeInsets.only(left: 20, right: 20, bottom: 20),
+        controller: controller,
+        physics: physics,
+        children: [
+          ...paths.map((path) => Container(
+              key: ValueKey(path),
+              margin: const EdgeInsets.only(bottom: 10),
+              padding: const EdgeInsets.only(
+                left: 10,
+                right: 10,
+                top: 5,
+                bottom: 5,
+              ),
+              decoration: BoxDecoration(
+                borderRadius: const BorderRadius.all(
+                  Radius.circular(12),
+                ),
+                border: Border.all(
+                  color: Theme.of(context)
+                      .colorScheme
+                      .outlineVariant
+                      .withOpacity(0.5),
+                  width: 1,
+                ),
+              ),
+              child: Row(children: [
+                Expanded(
+                    child: Text(
+                      path,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w500, fontSize: 14),
+                    )),
+                IconButton(
+                  icon: Icon(
+                    Symbols.delete,
+                    color: Theme.of(context).colorScheme.onSurfaceVariant,
+                    size: 20,
+                  ),
+                  onPressed: () {
+                    showDialog(
+                      context: context,
+                      barrierDismissible: false,
+                      builder: (BuildContext context) {
+                        ValueNotifier<bool> syncDelete =
+                        ValueNotifier<bool>(false);
+                        return AlertDialog(
+                          title: Row(
+                            crossAxisAlignment: CrossAxisAlignment.center,
+                            children: [
+                              Icon(
+                                Symbols.delete,
+                                color: Theme.of(context)
+                                    .colorScheme
+                                    .onSurfaceVariant,
+                                weight: 700,
+                              ),
+                              const SizedBox(
+                                width: 10,
+                              ),
+                              const Text('Delete Search Folder')
+                            ],
+                          ),
+                          contentPadding: const EdgeInsets.only(
+                            left: 20,
+                            right: 20,
+                            top: 20,
+                            bottom: 20,
+                          ),
+                          content: SizedBox(
+                              height: 100,
+                              child: Column(
+                                mainAxisSize: MainAxisSize.min,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children: [
+                                  const SizedBox(
+                                    width: 400,
+                                  ),
+                                  RichText(
+                                    text: TextSpan(
+                                      text: 'Are you sure you want to delete ',
+                                      style: TextStyle(
+                                        color: Theme.of(context)
+                                            .colorScheme
+                                            .onSurface,
+                                        fontWeight: FontWeight.w400,
+                                        fontSize: 14,
+                                      ),
+                                      children: [
+                                        TextSpan(
+                                          text: path,
+                                          style: TextStyle(
+                                            color: Theme.of(context)
+                                                .colorScheme
+                                                .onSurface,
+                                            fontWeight: FontWeight.w500,
+                                            fontSize: 14,
+                                          ),
+                                        ),
+                                      ],
+                                    ),
+                                  ),
+                                  const SizedBox(height: 20),
+                                  Row(
+                                    children: [
+                                      MouseRegion(
+                                        cursor: SystemMouseCursors.click,
+                                        child: GestureDetector(
+                                          onTap: () {
+                                            syncDelete.value =
+                                            !syncDelete.value;
+                                          },
+                                          child: const Text(
+                                              'Synchronized deletion index',
+                                              style: TextStyle(
+                                                fontSize: 12,
+                                              )),
+                                        ),
+                                      ),
+                                      ValueListenableBuilder(
+                                        valueListenable: syncDelete,
+                                        builder: (context, value, child) {
+                                          return Checkbox(
+                                              value: value,
+                                              onChanged: (value) {
+                                                syncDelete.value = value!;
+                                              });
+                                        },
+                                      ),
+                                    ],
+                                  )
+                                ],
+                              )),
+                          actions: [
+                            Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                TextButton(
+                                  onPressed: () {
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text('Cancel'),
+                                ),
+                                TextButton(
+                                  onPressed: () {
+                                    // context.read<SettingsController>().removeSearchFolder(path);
+                                    Navigator.of(context).pop();
+                                  },
+                                  child: const Text('Delete'),
+                                ),
+                              ],
+                            )
+                          ],
+                        );
+                      },
+                    );
+                    // context.read<SettingsController>().removeSearchFolder(path);
+                  },
+                )
+              ])))
+        ],
+      )),
+      selector: (_, settings) => settings.searchFolders,
     );
   }
 }
